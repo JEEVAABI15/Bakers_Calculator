7990b1710993675543a32eaaefaa008927e4c0d3
JEEVAABI15 <jeevaabishake2002@gmail.com>
2025-06-25 15:04:39 +0530

    backned testing file update-8.

--- src/server/server.js
+++ src/server/server.js
@@ -18,9 +18,11 @@ app.use(cors());
 app.use(express.json());
 
 // Database connection
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
+if (process.env.NODE_ENV !== 'test') {
+  mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
+}
 
 // Routes
 app.use('/api/users', usersRouter);
